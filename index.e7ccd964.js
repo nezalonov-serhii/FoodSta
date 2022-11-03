f0a2ff841e895c1041de53cf927d04dbe3456ffc
nezalonov-serhii <110600342+nezalonov-serhii@users.noreply.github.com>
2022-11-03 13:41:35 +0000

    Deploying to gh-pages from @ nezalonov-serhii/FoodSta@1656b03c41b513dd845b8a6c05b1ddae52b3d913 🚀

---
+++ index.e7ccd964.js
@@ -0,0 +1,2 @@
+new Swiper(".hero-slider",{slidesPerView:"auto",centeredSlides:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},observer:!0,observerParents:!0,observerChildren:!0,keyboard:{enabled:!0},mousewheel:{eventsTarget:".hero-slider"},speed:1e3,effect:"fade",fadeEffect:{crossFade:!0}});
+//# sourceMappingURL=index.e7ccd964.js.map
