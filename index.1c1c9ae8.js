47062c0c8c4908a2689ea8b7e39bfb69c75cd817
nezalonov-serhii <110600342+nezalonov-serhii@users.noreply.github.com>
2022-11-04 15:49:55 +0000

    Deploying to gh-pages from @ nezalonov-serhii/FoodSta@46ce7131d2f01a62b4a5a284564902f1f88f5ddd 🚀

---
+++ index.1c1c9ae8.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.1c1c9ae8.js.map
